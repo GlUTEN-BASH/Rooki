ec73885bba39bd3c5a6d770440dee7f4a4cdb969
GLUTEN <mironvlad58@mail.ru>
2024-01-06 12:39:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ pygame.mixer.init()
 
 theme = 0
 
-HEIGHT = 500
-WIDTH = 100
+HEIGHT = 650
+WIDTH = 170
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Нейрожест Beta 0.96')
@@ -44,7 +44,11 @@ letcount = 0
 word = 0
 letf = 0
 progress = []
+
 fl = False
+oldmode = False
+zerk = False
+
 astra = (197, 121, 255)
 astratext = (197, 255, 100)
 white = (255, 255, 255)
@@ -86,6 +90,10 @@ while settings:
                     theme = 1
                 case pygame.K_f:
                     fl = True
+                case pygame.K_b:
+                    oldmode = True
+                case pygame.K_v:
+                    zerk = True
                 case pygame.K_ESCAPE:
                     settings = False
                     running = True
@@ -93,7 +101,9 @@ while settings:
     screen.fill(back)
     screen.blit(my_font.render('0 - 5 - Выбор камеры', False, (text)), (0, 0)) 
     screen.blit(my_font.render('Нажмите ESC для продолжения', False, (text)), (0, 30))
-    screen.blit(my_font.render('Нажмите F для отзеркаливания', False, (text)), (0, 60))
+    screen.blit(my_font.render('Нажмите F для отзеркаливания камеры', False, (text)), (0, 60))
+    screen.blit(my_font.render('Нажмите B для вывода скелета', False, (text)), (0, 90))
+    screen.blit(my_font.render('Нажмите V для отзеркаливания вывода', False, (text)), (0, 120))
     pygame.display.flip()
 
 cap = cv2.VideoCapture(camnum)
@@ -160,10 +170,17 @@ while running:
 
         screen.fill((back)) 
 
-        frame_py = np.rot90(frame_rgb)
+        if oldmode:
+            frame_py = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_py = np.rot90(frame_py)
+        else:
+            frame_py = np.rot90(frame_rgb)
+
+
         fra = pygame.surfarray.make_surface(frame_py)
         fra = pygame.transform.scale(fra, (640, 480)) 
-        fra = pygame.transform.flip(fra, True, False) 
+        if not zerk:
+            fra = pygame.transform.flip(fra, True, False) 
         screen.blit(fra, (0,0))
         pygame.draw.rect(screen, back, pygame.Rect(10, 10, 70, 75))
         screen.blit(bigaboom.render(predicted_character, False, (text)), (12,0))
