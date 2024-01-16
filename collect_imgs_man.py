f032902e51899c55d54afccaa2a58a48bafb5707
GLUTEN <mironvlad58@mail.ru>
2024-01-16 14:37:45 +0300

    New Dataset + Some new funcs

--- collect_imgs_man.py
+++ collect_imgs_man.py
@@ -1,6 +1,12 @@
 import os
-
+import mediapipe as mp
+import numpy as np
 import cv2
+
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
 
 DATA_DIR = './data'
@@ -9,6 +15,8 @@ if not os.path.exists(DATA_DIR):
 
 number_of_classes = 32
 dataset_size = 100
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 cap = cv2.VideoCapture(0)
 i = int(input("ENTER CLASS: "))
@@ -22,9 +30,19 @@ for j in range(i, number_of_classes):
     while True:
         ret, frame = cap.read()
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
-        cv2.imshow('frame', frame)
         if cv2.waitKey(25) == ord('q'):
             break
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
+                    hand_landmarks,  # model output
+                    mp_hands.HAND_CONNECTIONS,  # hand connections
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+        cv2.imshow('frame', frame)
 
     counter = 0
     while counter < dataset_size:
@@ -34,6 +52,9 @@ for j in range(i, number_of_classes):
         cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
 
         counter += 1
+
+
+
 
 cap.release()
 cv2.destroyAllWindows()
